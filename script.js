4bf7c7cbd221855f2d3dbaca765b851fdc4da539
Tjebbe <tjebbemarchand@icloud.com>
2020-01-22 20:23:40 +0100

    Added background image and greeting message

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ function showTime() {
     // Output time.
     time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)}`;
 
+    // Call showTime every second.
     setTimeout(showTime, 1000);
 }
 
@@ -27,4 +28,26 @@ function addZero(number) {
     return (parseInt(number, 10) < 10 ? "0" : "") + number;
 }
 
+// Set background and greeting.
+function setBackgroundAndGreeting() {
+    let now = new Date();
+    let hour = now.getHours();
+
+    if (hour < 12) {
+        // Morning.
+        document.body.style.backgroundImage = "url('./img/morning.jpg')";
+        greeting.textContent = "Good Morning";
+    } else if (hour < 18) {
+        // Afternoon.
+        document.body.style.backgroundImage = "url('./img/afternoon.jpg')";
+        greeting.textContent = "Good Afternoon";
+    } else {
+        // Evening.
+        document.body.style.backgroundImage = "url('./img/evening.jpg')";
+        greeting.textContent = "Good Afternoon";
+        document.body.style.color = "white";
+    }
+}
+
 showTime();
+setBackgroundAndGreeting();
